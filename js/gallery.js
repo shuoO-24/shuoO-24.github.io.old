e6de3bbb0c43e7dd4250acc9805543c8f1357fdf
shuoO-24 <lishuo2332@gmail.com>
2021-06-30 20:15:33 -0700

    Site updated: 2021-06-30 20:15:33

--- js/gallery.js
+++ js/gallery.js
@@ -2,13 +2,15 @@
   // Caption
   $('.entry').each(function(i){
     $(this).find('img').each(function(){
-      var alt = this.alt;
+      if (!$(this).parent().is("a")) {
+        var alt = this.alt, title = this.title;
+        var caption = title ? title : alt;
+        if (caption){
+          $(this).after('<div class="caption"><i  class="fa fa-picture-o"></i>' + caption + '</div>');
+        }
 
-      if (alt){
-        $(this).after('<span class="caption">' + alt + '</span>');
+        $(this).wrap('<a href="' + this.src + '" title="' + caption + '" class="fancybox" rel="gallery' + i + '" />');
       }
-
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox" rel="gallery' + i + '" />');
     });
   });
 
